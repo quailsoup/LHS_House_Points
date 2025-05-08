74478b7e91661ca79e38ca2441b7d6b27b0e9e13
Kaitlyn <gibsonk@student.lincoln.school.nz>
2025-05-08 13:54:32 +1200

    added files

---
+++ 13DTP/index.js
@@ -0,0 +1,186 @@
+// Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyDGkNus0BpWcV68Xhcgl-iWkHNmYbaLDiU",
+    authDomain: "house-points-83688.firebaseapp.com",
+    databaseURL: "https://house-points-83688-default-rtdb.firebaseio.com",
+    projectId: "house-points-83688",
+    storageBucket: "house-points-83688.firebasestorage.app",
+    messagingSenderId: "706900936679",
+    appId: "1:706900936679:web:21e15da81946fb41b29f42",
+    measurementId: "G-83FTQPYTEL"
+  };
+
+  // Initialise Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+
+  const patikiInput = document.getElementById('patiki-input');
+  const kuakaInput = document.getElementById('kuaka-input');
+  const kotareInput = document.getElementById('kotare-input');
+  const kahuInput = document.getElementById('kahu-input');
+  const makoInput = document.getElementById('mako-input');
+  const inakaInput = document.getElementById('inaka-input');
+
+  const patikiCountDisplay = document.getElementById('patiki-count');
+  const kuakaCountDisplay = document.getElementById('kuaka-count');
+  const kotareCountDisplay = document.getElementById('kotare-count');
+  const kahuCountDisplay = document.getElementById('kahu-count');
+  const makoCountDisplay = document.getElementById('mako-count');
+  const inakaCountDisplay = document.getElementById('inaka-count');
+
+  const patikiRef = db.ref('votes/patiki');
+  const kuakaRef = db.ref('votes/kuaka');
+  const kotareRef = db.ref('votes/kotare');
+  const kahuRef = db.ref('votes/kahu');
+  const makoRef = db.ref('votes/mako');
+  const inakaRef = db.ref('votes/inaka');
+
+  let patikiCount = 0;
+  let kuakaCount = 0;
+  let kotareCount = 0;
+  let kahuCount = 0;
+  let makoCount = 0;
+  let inakaCount = 0;
+
+  // Load house point count
+  patikiRef.on('value', snapshot => {
+    patikiCount = snapshot.val() || 0;
+    patikiCountDisplay.textContent = patikiCount;
+  });
+  kuakaRef.on('value', snapshot => {
+    kuakaCount = snapshot.val() || 0;
+    kuakaCountDisplay.textContent = kuakaCount;
+  });
+  kotareRef.on('value', snapshot => {
+    kotareCount = snapshot.val() || 0;
+    kotareCountDisplay.textContent = kotareCount;
+  });
+  kahuRef.on('value', snapshot => {
+    kahuCount = snapshot.val() || 0;
+    kahuCountDisplay.textContent = kahuCount;
+  });
+  makoRef.on('value', snapshot => {
+    makoCount = snapshot.val() || 0;
+    makoCountDisplay.textContent = makoCount;
+  });
+  inakaRef.on('value', snapshot => {
+    inakaCount = snapshot.val() || 0;
+    inakaCountDisplay.textContent = inakaCount;
+  });
+
+
+  // Submit patiki house points
+  document.getElementById('submit-patiki').addEventListener('click', () => {
+    const inputVal = parseInt(patikiInput.value);
+    
+    if (isNaN(inputVal)) {
+      alert("Please enter a valid number of house points.");
+      return;
+      // allow a negative number to be entered
+    } else if (inputVal < 0) {
+      var confirmed = confirm("Are you sure you want to enter a negative number?")
+      if (confirmed === false) {
+        return;
+      }
+    }
+    
+    patikiRef.transaction(current => (current || 0) + inputVal);
+    patikiInput.value = ""; // Clears the box after input   
+});
+
+// Submit kuaka house points
+document.getElementById('submit-kuaka').addEventListener('click', () => {
+    const inputkuakaVal = parseInt(kuakaInput.value);
+    
+    if (isNaN(inputkuakaVal)) {
+        alert("Please enter a valid number of house points.");
+        return;
+        // allow a negative number to be entered
+    } else if (inputkuakaVal < 0) {
+      var confirmed = confirm("Are you sure you want to enter a negative number?")
+      if (confirmed === false) {
+        return;
+      }
+    }
+    
+    kuakaRef.transaction(current => (current || 0) + inputkuakaVal);
+    kuakaInput.value = ""; // Clears the box after input
+});
+
+// Submit kotare house points
+document.getElementById('submit-kotare').addEventListener('click', () => {
+  const inputkotareVal = parseInt(kotareInput.value);
+  
+  if (isNaN(inputkotareVal)) {
+      alert("Please enter a valid number of house points.");
+      return;
+      // allow a negative number to be entered
+  } else if (inputkotareVal < 0) {
+    var confirmed = confirm("Are you sure you want to enter a negative number?")
+    if (confirmed === false) {
+      return;
+    }
+  }
+  
+  kotareRef.transaction(current => (current || 0) + inputkotareVal);
+  kotareInput.value = ""; // Clears the box after input
+});
+
+// Submit kahu house points
+document.getElementById('submit-kahu').addEventListener('click', () => {
+  const inputkahuVal = parseInt(kahuInput.value);
+  
+  if (isNaN(inputkahuVal)) {
+      alert("Please enter a valid number of house points.");
+      return;
+      // allow a negative number to be entered
+  } else if (inputkahuVal < 0) {
+    var confirmed = confirm("Are you sure you want to enter a negative number?")
+    if (confirmed === false) {
+      return;
+    }
+  }
+  
+  kahuRef.transaction(current => (current || 0) + inputkahuVal);
+  kahuInput.value = ""; // Clears the box after input
+});
+
+// Submit mako house points
+document.getElementById('submit-mako').addEventListener('click', () => {
+  const inputmakoVal = parseInt(makoInput.value);
+  
+  if (isNaN(inputmakoVal)) {
+      alert("Please enter a valid number of house points.");
+      return;
+      // allow a negative number to be entered
+  } else if (inputmakoVal < 0) {
+    var confirmed = confirm("Are you sure you want to enter a negative number?")
+    if (confirmed === false) {
+      return;
+    }
+  }
+  
+  makoRef.transaction(current => (current || 0) + inputmakoVal);
+  makoInput.value = ""; // Clears the box after input
+});
+
+
+// Submit inaka house points
+document.getElementById('submit-inaka').addEventListener('click', () => {
+  const inputinakaVal = parseInt(inakaInput.value);
+  
+  if (isNaN(inputinakaVal)) {
+      alert("Please enter a valid number of house points.");
+      return;
+      // allow a negative number to be entered
+  } else if (inputinakaVal < 0) {
+    var confirmed = confirm("Are you sure you want to enter a negative number?")
+    if (confirmed === false) {
+      return;
+    }
+  }
+  // add to house points already submitted
+  inakaRef.transaction(current => (current || 0) + inputinakaVal);
+  inakaInput.value = ""; // Clears the box after input
+
+});
